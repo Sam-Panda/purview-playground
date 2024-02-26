8cf24dcb64e3428210c3662bed0f254a0c96fc48
Sam <samarendra.panda@outlook.com>
2024-02-26 22:22:44 +0530

    .

--- data_source_scanning/scoped_scanning_from_lookup_file.py
+++ data_source_scanning/scoped_scanning_from_lookup_file.py
@@ -5,27 +5,42 @@ from azure.identity import ClientSecretCredential
 from azure.core.exceptions import HttpResponseError
 import os
 from dotenv import load_dotenv
-from util.helper import Intialization 
 import pandas as pd
+from azure.identity import AzureCliCredential
 
-
-i = Intialization()
-subscription_id = i.subscription_id
-purview_account_name = i.reference_name_purview
-
+load_dotenv()
+# reading the variables from .env file  
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
+tenant_id = os.getenv("TENANT_ID")
+reference_name_purview = os.getenv("PURVIEW_ACCOUNT_NAME")
+subscription_id = os.getenv("SUBSCRIPTION_ID")
+resource_endpoint = os.getenv("RESOURCE_ENDPOINT")
+databasename = os.getenv("DATABASE_NAME")
 #declare variables 
 
-datasource_name = "ds4mapi_azuresqlserver_opnhckdasqlsvr"
-resource_endpoint = "opnhckdasqlsvr.database.windows.net"
-resource_id = f"/subscriptions/{subscription_id}/resourceGroups/openhack_data_analytics/providers/Microsoft.Sql/servers/opnhckdasqlsvr"
-reference_name_purview = purview_account_name
-databaseName = "opnhckdasqldb"
+purview_registered_datasource_name = os.getenv("PURVIEW_REGISTERED_DATASOURCE_NAME")
 
+
+def get_credentials():
+    # credentials = ClientSecretCredential(client_id=client_id, client_secret=client_secret, tenant_id=tenant_id)
+    credentials = AzureCliCredential()
+    return credentials
+
+def get_admin_client(reference_name_purview):
+    credentials = get_credentials()
+    client = PurviewAccountClient(endpoint=f"https://{reference_name_purview}.purview.azure.com/", credential=credentials, logging_enable=True)
+    return client
+
+def get_purview_client(reference_name_purview):
+    credentials = get_credentials()
+    client = PurviewScanningClient(endpoint=f"https://{reference_name_purview}.scan.purview.azure.com", credential=credentials, logging_enable=True)  
+    return client
 
 
 def get_collection_Id(collection_name):
     try:
-        client = i.get_admin_client()
+        client = get_admin_client(reference_name_purview)
     except ValueError as e:
         print(e)
     collection_name_unique_id = ''
@@ -44,29 +59,35 @@ def get_collection_Id(collection_name):
 def create_default_scan (scan_name, collection_name_unique_id):
 
     body_input = {
-            "kind": "AzureSqlDatabaseMsi",
+            "kind": "AzureSqlDatabaseCredential",
             "properties": {
 
                 "collection": {
                     "type": "CollectionReference",
                     "referenceName": collection_name_unique_id
                 },
-                "enableLineage": True,
+                "credential": {
+                        "referenceName": "sapa-WideWorldImporters-cred",
+                        "credentialType": "SqlAuth"
+                },
+
+                "enableLineage": False,
                 "recurrenceInterval":None,
                 "serverEndpoint": resource_endpoint,
-                "databaseName": databaseName,
+                "databaseName": databasename,
                 "scanRulesetName":"AzureSqlDatabase",
                 "scanRulesetType":"System"
             }
     }
+    print (body_input)
     try:
-        client = i.get_purview_client()
+        client = get_purview_client(reference_name_purview)
     except ValueError as e:
         print(e)
 
 
     try:
-        response = client.scans.create_or_update(data_source_name=datasource_name, scan_name=scan_name, body=body_input)
+        response = client.scans.create_or_update(data_source_name=purview_registered_datasource_name, scan_name=scan_name, body=body_input)
         print(response)
     except HttpResponseError as e:
         print(e)
@@ -74,7 +95,7 @@ def create_default_scan (scan_name, collection_name_unique_id):
 def apply_filter_on_scan(scan_name, lstexcludeUriPrefixes, lstincludeUriPrefixes):
     
     try:
-        client = i.get_purview_client()
+        client = get_purview_client(reference_name_purview)
     except ValueError as e:
         print(e)
 
@@ -86,7 +107,8 @@ def apply_filter_on_scan(scan_name, lstexcludeUriPrefixes, lstincludeUriPrefixes
         "includeRegexes":None
         }
      }
-    client.filters.create_or_update(data_source_name=datasource_name, scan_name=scan_name, body=filter_body_input)
+    print (filter_body_input)
+    client.filters.create_or_update(data_source_name=purview_registered_datasource_name, scan_name=scan_name, body=filter_body_input)
 
 
 
@@ -96,19 +118,20 @@ file_path = os.path.join(os.getcwd(),"InputFiles", "assestsToBeScanned.csv")
 df_csv = pd.read_csv(file_path)
 unique_scan_names = df_csv["scanName"].unique()
 for scan_name in unique_scan_names:
-    lst_unique_qualified_names = df_csv["assetqualifiedDomain"].unique().tolist()
-    df_csv_filtered = df_csv.query(f"scanName == '{scan_name}'")
-    collection_name = df_csv_filtered["collectionName"].unique()[0]
-    collection_name_unique_id = get_collection_Id(collection_name)
-    lst_included_qualified_names = df_csv_filtered["assetqualifiedDomain"].unique().tolist()
-    lst_excluded_qualified_names = list(set(lst_unique_qualified_names) - set(lst_included_qualified_names))
-    create_default_scan(
-        scan_name=scan_name,
-        collection_name_unique_id=collection_name_unique_id)
-    apply_filter_on_scan(
-        scan_name=scan_name,
-        lstexcludeUriPrefixes=lst_excluded_qualified_names,
-        lstincludeUriPrefixes=lst_included_qualified_names
-    )
+    if scan_name != "exclude":  
+        lst_unique_qualified_names = df_csv["assetqualifiedDomain"].unique().tolist()
+        df_csv_filtered = df_csv.query(f"scanName == '{scan_name}'")
+        collection_name = df_csv_filtered["collectionName"].unique()[0]
+        collection_name_unique_id = get_collection_Id(collection_name)
+        lst_included_qualified_names = df_csv_filtered["assetqualifiedDomain"].unique().tolist()
+        lst_excluded_qualified_names = list(set(lst_unique_qualified_names) - set(lst_included_qualified_names))
+        create_default_scan(
+            scan_name=scan_name,
+            collection_name_unique_id=collection_name_unique_id)
+        apply_filter_on_scan(
+            scan_name=scan_name,
+            lstexcludeUriPrefixes=lst_excluded_qualified_names,
+            lstincludeUriPrefixes=lst_included_qualified_names
+        )
 
 
